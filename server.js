1ad769ba2589124cf9be365cc521c245a6d0ed10
justo <justoelhumilde@gmail.com>
2020-01-13 05:51:59 +0100

    agaporni

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ function chatea(client){
     function htmlEntities(str) {
         return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
                           .replace(/>/g, '&gt;').replace(/"/g, '&quot;')
-                          .replace(/(https?:\/\/[^\s]*\.(?:jpg|jpeg|gif|png))/g, '<img src="$1">');
-                          //.replace(/https?:\/\/(?![^\"\s]*(?:jpg|png|gif))[^\"\s]+/g, '<a href="$1">$1</a>');
+                          .replace(/(https?:\/\/[^\s]*\.(?:jpg|jpeg|gif|png))/g, '<img src="$1">')
+                          .replace(/https?:\/\/(?![^\"\s]*(?:jpg|png|gif))[^\"\s]+/g, '<a href="$&">$&</a>');
     }
 
     // Array with some colors
